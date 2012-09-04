b348249135338d11ab9ed39dccd3d32d4e834538
arne.krawie@gmail.com <arne.krawie@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-04 13:39:28 +0000

    Engin hat die falschen Dateien eingecheckt .. hier nun die korrekten :)

--- trunk/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ trunk/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -15,5 +15,15 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
