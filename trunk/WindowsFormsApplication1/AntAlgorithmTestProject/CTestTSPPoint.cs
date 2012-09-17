b2d85e7129cfd4680f132a3579ebd7d0a4232361
arne.krawie@gmail.com <arne.krawie@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 10:58:54 +0000

    CTSPPointList um Methode zum iterieren mit foreach erweitert Testprojekt für Komponententests eingefügt

---
+++ trunk/WindowsFormsApplication1/AntAlgorithmTestProject/CTestTSPPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WindowsFormsApplication1;
+
+namespace AntAlgorithmTestProject
+{
+    [TestClass]
+    public class CTestTSPPoint
+    {
+        [TestMethod]
+        public void constructorTest()
+        {
+            CTSPPoint testPoint = new CTSPPoint("");
+
+            testAPoint(testPoint, 0, 0, "");
+        }
+
+        protected void testAPoint(CTSPPoint point, int expectedX, int expectedY, string expectedLabel)
+        {
+            Assert.IsTrue(point.x == expectedX, "X-Koordinate von CTSPPoint falsch");
+            Assert.IsTrue(point.y == expectedY, "Y-Koordinate von CTSPPoint falsch");
+            Assert.IsTrue(point.getLabel() == expectedLabel, "Label von CTSPPoint falsch");
+        }
+    }
+}
