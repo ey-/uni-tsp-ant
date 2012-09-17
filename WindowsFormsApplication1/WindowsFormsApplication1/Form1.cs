2cbecfedd5cdf880d7892d384e3e37ef5df65fcb
janzimmeranne@gmail.com <janzimmeranne@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 14:10:32 +0000

    Gui anpassung 17.09 16:10

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -11,6 +11,11 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private double heuristicPheromonUpdateValue =0.001;
+        private double pheromonValue=0.001;
+        private double initialPheromonValue=0.001;
+        private double humidificationValue=0.001;
+        private double heuristicValue=0.001;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +25,20 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Create the ToolTip and associate with the Form container.
+            ToolTip toolTip1 = new ToolTip();
+
+            // Set up the delays for the ToolTip.
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+            
+
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip1.SetToolTip(this.checkBox3, "Diese Option ist nur verfügbar, wenn eine *.opt.tour-Datei für die *.tsp-Datei vorhanden ist. Diese Datei sollte sich in dem selben Ordner befinden.");
+            //toolTip1.SetToolTip(this.chec, "My checkBox1");
 
         }
 
@@ -28,19 +47,6 @@ namespace WindowsFormsApplication1
             Application.Exit();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (comboBox1.SelectedIndex == 1)
-            { 
-                tThreshold.Visible = true;
-                labelThreshold.Visible = true;
-            }
-            else {
-                tThreshold.Visible = false;
-                labelThreshold.Visible = false;
-            }
-                
-        }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
@@ -52,31 +58,31 @@ namespace WindowsFormsApplication1
 
         private void trackBarPheremon_Scroll(object sender, EventArgs e)
         {
-            double pheromonValue = ((double)trackBarPheromon.Value / 1000);
+            pheromonValue = ((double)trackBarPheromon.Value / 1000);
             labelPheremon.Text = pheromonValue.ToString();
         }
 
         private void trackBarheuristic_Scroll(object sender, EventArgs e)
         {
-            double heuristicValue = ((double)trackBarheuristic.Value / 1000);
+            heuristicValue = ((double)trackBarheuristic.Value / 1000);
             labelHeuristic.Text = heuristicValue.ToString();
         }
 
         private void trackBarHumidification_Scroll(object sender, EventArgs e)
         {
-            double humidificationValue = ((double)trackBarHumidification.Value / 1000);
+            humidificationValue = ((double)trackBarHumidification.Value / 1000);
             labelHumidification.Text = humidificationValue.ToString();
         }
 
         private void trackBarinitialPheromon_Scroll(object sender, EventArgs e)
         {
-            double initialPheromonValue = ((double)trackBarinitialPheromon.Value / 1000);
+            initialPheromonValue = ((double)trackBarinitialPheromon.Value / 1000);
             labelinitialPheromon.Text = initialPheromonValue.ToString();
         }
 
         private void trackBarheuristicPheromonUpdate_Scroll(object sender, EventArgs e)
         {
-            double heuristicPheromonUpdateValue = ((double)trackBarheuristicPheromonUpdate.Value / 1000);
+            heuristicPheromonUpdateValue = ((double)trackBarheuristicPheromonUpdate.Value / 1000);
             labelheuristicPheromonUpdate.Text = heuristicPheromonUpdateValue.ToString();
         }
 
@@ -97,6 +103,34 @@ namespace WindowsFormsApplication1
                 mRenderWindow.Refresh();
 
             }
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            CAntAlgorithmParameters.getInstance().numberAnts = Convert.ToInt32(uAntsQuantity.Value);
+            CAntAlgorithmParameters.getInstance().numberMaxIterations = Convert.ToInt32(uQuantityIterations.Value);
+            CAntAlgorithmParameters.getInstance().pheromoneParameter = pheromonValue;
+            CAntAlgorithmParameters.getInstance().pheromoneUpdate = heuristicPheromonUpdateValue;
+            CAntAlgorithmParameters.getInstance().initialPheromone = initialPheromonValue;
+            CAntAlgorithmParameters.getInstance().localInformation = heuristicValue;
+            CAntAlgorithmParameters.getInstance().evaporationFactor = humidificationValue;
+            MessageBox.Show("Ants: " + CAntAlgorithmParameters.getInstance().numberAnts + "\n" + CAntAlgorithmParameters.getInstance().numberMaxIterations + "\n" + CAntAlgorithmParameters.getInstance().pheromoneParameter + " \n" + "usw usw");
+        }
+
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                tThreshold.Visible = true;
+                labelThreshold.Visible = true;
+            }
+            else
+            {
+                tThreshold.Visible = false;
+                labelThreshold.Visible = false;
+            }
+
         }
 
     }
