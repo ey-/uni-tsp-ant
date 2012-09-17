f5c9f1d44330cceffbf0b43b11842c387beb4212
janzimmeranne@gmail.com <janzimmeranne@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 10:39:00 +0000

    möglicherweise nicht lauffähig

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/CTour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class CTour
+    {
+
+        private CTSPPointList _points;
+        private double _tourLength;
+
+
+        public void Add(CTSPPoint point)
+        {
+            if (_points.length() == 0)
+                _tourLength = 0;
+            else
+                _tourLength = _tourLength + CConnectionList.getInstance().getConnection(_points.getPoint(_points.length()-1),point).getDistance();
+            _points.addPoint(point);
+        }
+
+        public CTSPPoint GetPoint(int index)
+        {
+            return _points.getPoint(index);
+        }
+
+        public double Length
+        {
+            get
+            {
+                return _tourLength;
+            }
+        }
+
+
+
+    }
+}
