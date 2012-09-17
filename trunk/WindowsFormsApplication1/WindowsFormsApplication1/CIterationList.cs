a0fa3ac25522560379c926892231fe9470850db8
janzimmeranne@gmail.com <janzimmeranne@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 10:39:00 +0000

    möglicherweise nicht lauffähig

---
+++ trunk/WindowsFormsApplication1/WindowsFormsApplication1/CIterationList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class CIterationList
+    {
+        private List<CIteration> _iterationList =  new List<CIteration>();
+
+        public int Add(CIteration iteration)
+        {
+            _iterationList.Add(iteration);
+            return _iterationList.Count();
+        }
+
+        public void Clear()
+        {
+            _iterationList.Clear();
+        }
+
+        public List<CIteration> Instance()
+        {
+            return _iterationList;
+        }
+
+        public CIteration Last()
+        {
+            return _iterationList.Last();
+        }
+
+        public double GlobalAverageTourLength()
+        {
+            var avg = 0.0;
+            for (var i = 0; i < _iterationList.Count; i++)
+                avg += (_iterationList[i].AverageTourLength / _iterationList.Count);
+            return avg;
+        }
+
+        public CTour GlobalBestTour()
+        {
+            CTour bestTour = _iterationList[0].ShortestTour;
+            for (var i = 1; i < _iterationList.Count; i++)
+            {
+                var currentTour = _iterationList[i].ShortestTour;
+                if (currentTour.Length < bestTour.Length)
+                    bestTour = currentTour;
+            }
+            return bestTour;
+        }
+    }
+}
