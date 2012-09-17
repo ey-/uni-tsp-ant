9b2a0ec99118c3d9b161a83a17bd430d04a5c7ae
ey.engin.1991@gmail.com <ey.engin.1991@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 12:23:06 +0000

    TSPLibFileParserTest

---
+++ trunk/WindowsFormsApplication1/AntAlgorithmTestProject/CTestTSPLibFileParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+
+namespace AntAlgorithmTestProject
+{
+    [TestClass]
+    public class CTestTSPLibFileParser
+    {
+        [TestMethod]
+        public void constructorFileParserTest()
+        {
+            string fileAdress = "../../ALL_tsp/berlin52.tsp/berlin52.tsp";
+            testTSPFileParser(fileAdress, 0, 565,575);
+        }
+
+        protected void testTSPFileParser(string fileAdress,int pointToCheck,int expectedX,int expectedY)
+        {
+            Stream file =new FileStream(fileAdress, FileMode.Open);
+            CTSPLibFileParser fileParser = new CTSPLibFileParser(file);
+            fileParser.fillTSPPointList();
+            file.Close();
+            CTSPPoint readPoint=CTSPPointList.getInstance().getPoint(pointToCheck);
+
+            Assert.IsTrue(expectedX == readPoint.x, "X-Wert wurde falsch eingelesen");
+            Assert.IsTrue(expectedY == readPoint.y, "Y-Wert wurde falsch eingelesen");
+                
+        }
+    }
+}
