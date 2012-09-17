356b769791723859c8ffa5669ca30b6f5ef6fdf1
arne.krawie@gmail.com <arne.krawie@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 11:58:04 +0000

    CTestTSPPointList.cs eingefügt Fehler behoben: CVector2f wird nicht mehr verwendet

---
+++ trunk/WindowsFormsApplication1/AntAlgorithmTestProject/CTestTSPPointList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+
+namespace AntAlgorithmTestProject
+{
+    [TestClass]
+    public class CTestTSPPointList
+    {
+        [TestMethod]
+        public void singletonTest()
+        {
+            CTSPPointList singletonObject = CTSPPointList.getInstance();
+
+
+        }
+    }
+}
