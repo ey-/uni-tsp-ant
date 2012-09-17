3e352a9bd106e1d2bf9ecde469abe7a6c965c381
janzimmeranne@gmail.com <janzimmeranne@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-09-17 12:53:07 +0000

    CAnt CAntAlgorithem.cs CAntAlgorithmParameters.cs wurde hinzugefügft und mit inhalt gefüllt. CAntAlgorithem.cs kann nun den entscheiden zu welchem Punkt als nächstes gelaufen werden soll. In CAntAlgorithmParameters.cs werden die Parameter aus der Gui gespeichert.

--- WindowsFormsApplication1/WindowsFormsApplication1/CAnt.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/CAnt.cs
@@ -7,62 +7,10 @@ namespace WindowsFormsApplication1
 {
     class CAnt
     {
-        private CTSPPointList _pointsVisited;
-        private CTSPPointList _pointsToVisit;
-        private double _pheromon;
+        private CTSPPointList  pointsToVisit;
+        private double antsPheromone;
+        private CTour tourOfTheAnt;
+        private CTSPPoint currentPoint;
 
-        private CTSPPoint _currentPoint
-        {
-            get
-            {
-                return _pointsVisited.getPoint(_pointsVisited.length() - 1);
-            }
-            set
-            {
-                _pointsVisited.addPoint(value);
-                _pointsToVisit.delete(value);
-            }
-        }
-
-        public CAnt(ref CTSPPointList points, double pheromon)
-        {
-            var random = new Random();
-            _pointsToVisit = points.copy();
-            _currentPoint = points.getPoint(random.Next((int) points.length()));
-        }
-        
-        public CTSPPointList VisitedPoints
-        {
-            get
-            {
-                return _pointsVisited;
-            }
-        }
-
-        public void GoToNextPoint()
-        {
-            double dChanceValue;
-            double dPheromone = 1;
-            double dSumChanceValue = 0;
-            double dBetha = 10;
-            double dAlpha = 0;
-            double currentBestDistance = 0;
-
-            /*foreach (var p in _pointsToVisit)
-            {
-                var distance = CConnectionList.getInstance().getConnection(_currentPoint,p).getDistance();
-                dSumChanceValue = Math.Pow((1 / distance), dAlpha);
-  
-                dChanceValue = ((Math.Pow(dPheromone, dAlpha)) * (Math.Pow((1 / distance), dBetha)));
-                if (((dChanceValue / dSumChanceValue) > currentBestDistance))
-                {
-                    currentBestDistance = distance;
-                    _currentPoint = p;
-                }
-
-            }*/
-
-
-        }
     }
 }
