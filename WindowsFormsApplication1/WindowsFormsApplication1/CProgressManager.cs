7f5c6f2a2c1fd080e10f4f52a2077d0ca491a6ab
arne.krawie@gmail.com <arne.krawie@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-10-10 16:33:26 +0000

    fehlende Datei CProgressManager.cs zum Projekt hinzugefügt Dummydatei für CTestAnt.cs eingefügt

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/CProgressManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class CProgressManager
+    {
+        protected static ProgressBar mManagedProgressbar = null;
+        protected static int mIncrementPerSteps = 1;
+        protected static long mStepsCounted = 0;
+
+        public static void setProgressBar(ProgressBar progressBar)
+        {
+            mManagedProgressbar = progressBar;
+            mManagedProgressbar.Minimum = 0;
+        }
+
+        public static void setStepsPointsAndConnections(int numberPoints)
+        {
+            int steps = numberPoints; // erstellen der Punkte
+            steps += numberPoints * (numberPoints - 1) / 2; // erstellen der Verbindungen
+
+            calculcateIncrementPerSteps(steps);
+            mStepsCounted = 0;
+
+            if (mManagedProgressbar != null)
+            {
+                mManagedProgressbar.Invoke(new Action(delegate()
+                    {
+                        mManagedProgressbar.Maximum = steps;
+                        mManagedProgressbar.Step = 0;
+                    }));
+            }
+        }
+
+        public static void setStepsConnections(int numberPoints)
+        {
+            calculcateIncrementPerSteps(numberPoints);
+            mStepsCounted = 0;
+
+            if (mManagedProgressbar != null)
+            {
+                mManagedProgressbar.Invoke(new Action(delegate()
+                {
+                    mManagedProgressbar.Maximum = numberPoints * (numberPoints - 1) / 2;
+                    mManagedProgressbar.Step = 0;
+                }));
+            }
+        }
+
+        public static void setStepsIteration(int numberIterations)
+        {
+            calculcateIncrementPerSteps(numberIterations);
+            mStepsCounted = 0;
+
+            if (mManagedProgressbar != null)
+            {
+                mManagedProgressbar.Invoke(new Action(delegate()
+                {
+                    mManagedProgressbar.Maximum = numberIterations;
+                    mManagedProgressbar.Step = 0;
+                }));
+            }
+        }
+
+        protected static void calculcateIncrementPerSteps(int steps)
+        {
+            mIncrementPerSteps = steps / 100;
+            if (mIncrementPerSteps == 0)
+            {
+                mIncrementPerSteps = 1;
+            }
+        }
+
+        public static void stepDone()
+        {
+
+            if (mStepsCounted % mIncrementPerSteps == 0)
+            {
+                if (mManagedProgressbar != null)
+                {
+                    mManagedProgressbar.Invoke(new Action(delegate()
+                        {
+                            mManagedProgressbar.Increment(mIncrementPerSteps);
+                        }));
+                }
+            }
+            mStepsCounted++;
+        }
+
+    }// class
+} // namespace
