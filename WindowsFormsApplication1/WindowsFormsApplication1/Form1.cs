4bfd6a32b748a0a305be44433f693afbebb25a8b
ey.engin.1991@gmail.com <ey.engin.1991@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-10-24 09:27:57 +0000

    some Code-Refactoring + change Color of bestPathOfIteration

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -60,25 +60,7 @@ namespace WindowsFormsApplication1
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-            // Create the ToolTip and associate with the Form container.
-            ToolTip toolTip1 = new ToolTip();
-
-            // Set up the delays for the ToolTip.
-            toolTip1.AutoPopDelay = 5000;
-            toolTip1.InitialDelay = 1000;
-            toolTip1.ReshowDelay = 500;
-            // Force the ToolTip text to be displayed whether or not the form is active.
-            toolTip1.ShowAlways = true;
-            
-
-            // Set up the ToolTip text for the Button and Checkbox.
-            toolTip1.SetToolTip(this.cStoppLoesung, "Diese Option ist nur verfügbar, wenn eine *.opt.tour-Datei für die *.tsp-Datei vorhanden ist. Diese Datei sollte sich in dem selben Ordner befinden.");
-            //toolTip1.SetToolTip(this.chec, "My checkBox1");
-
-        }
 
         private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -356,6 +338,7 @@ namespace WindowsFormsApplication1
         private void drawSettings_CheckedChanged(object sender, EventArgs e)
         {
             mRenderWindow.setDrawSettings(cBallConnection.Checked, cBoptPath.Checked, cBbestPathOfIteration.Checked, cBbestPathOfAllIteration.Checked);
+            mRenderWindow.Refresh();
         }
 
 
