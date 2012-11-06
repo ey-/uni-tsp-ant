0e3503bce862da982ce670e8ede61551b27a0aaa
arne.krawie@gmail.com <arne.krawie@gmail.com@75d281b1-22b1-5e79-d6a2-f5c18bc89b87>
2012-11-06 19:53:38 +0000

    Finales Release

---
+++ trunk/rel-3/WindowsFormsApplication1/WindowsFormsApplication1/CInsufficientMemoryException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class CInsufficientMemoryException : Exception
+    {
+        public enum E_EXCEPTION_TYPE
+        {
+            E_RAM_SIZE_ERROR,
+            E_32_BIT_ERROR
+        };
+
+        E_EXCEPTION_TYPE mType;
+        long mMemoryNeeded;
+        long mMemoryAvailable;
+
+        public CInsufficientMemoryException(E_EXCEPTION_TYPE type, long memoryNeeded, long memoryAvailable = 0)
+        {
+            mType = type;
+            mMemoryNeeded = memoryNeeded;
+            mMemoryAvailable = memoryAvailable;
+        }
+
+        public E_EXCEPTION_TYPE getType()
+        {
+            return mType;
+        }
+
+        public long getMemoryNeeded()
+        {
+            return mMemoryNeeded;
+        }
+
+        public long getMemoryAvailable()
+        {
+            return mMemoryAvailable;
+        }
+    }
+}
